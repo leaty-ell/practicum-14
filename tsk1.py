c5e9f04423f5a602822868733d78c87f327e5fbc
leaty-ell <lisalimanova14@gmail.com>
2025-11-10 20:24:45 +0700

    Create tsk1.py

---
+++ tsk1.py
@@ -0,0 +1,13 @@
+numbers = []
+for i in range(10):
+    num = int(input())
+    numbers.append(num)
+
+new_list=[]
+for i in range(8): 
+    sum_neighbors = numbers[i] + numbers[i+1]
+    new_list.append(sum_neighbors)
+
+print("Исходный список:", numbers)
+print("Новый список:", new_list)
+
