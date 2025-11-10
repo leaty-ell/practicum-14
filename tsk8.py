4a8848df71e422f7b6a3366a64efee47308ed40a
leaty-ell <lisalimanova14@gmail.com>
2025-11-10 20:57:32 +0700

    Update tsk8.py

--- tsk8.py
+++ tsk8.py
@@ -1,9 +1,24 @@
 def sort_string(s):
+    """Sort characters in a string in alphabetical order.
+    Args:
+        s (str): The input string to be sorted.
+    Returns:
+        str: The sorted string with characters in alphabetical order.
+    """
     char_list = list(s)
     char_list.sort()
     sorted_string = ''.join(char_list)
     return sorted_string
 
-input_string = input("Введите строку: ")
-result = sort_string(input_string)
-print("Отсортированная строка:", result)
+
+def main():
+    """
+    Main program function.
+    """
+    input_string = input("Введите строку: ")
+    result = sort_string(input_string)
+    print("Отсортированная строка:", result)
+
+
+if __name__ == "__main__":
+    main()
