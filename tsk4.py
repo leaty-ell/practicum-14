c8e53f7407af5b696671bcc5c84c70811bd6c1a9
leaty-ell <lisalimanova14@gmail.com>
2025-11-11 08:55:43 +0700

    Update tsk4.py

--- tsk4.py
+++ tsk4.py
@@ -5,7 +5,6 @@ signs = ".,!?;:()\"'"
 
 for word in words:
     clean_word = word.strip(signs).lower()
-    
     if clean_word and clean_word not in unique_words:
         unique_words.append(clean_word)
 
