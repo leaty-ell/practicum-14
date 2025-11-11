c1f285b7dbcfe30544884fb59fca6cfcae47181a
leaty-ell <lisalimanova14@gmail.com>
2025-11-11 09:48:34 +0700

    Update tsk10.py

--- tsk10.py
+++ tsk10.py
@@ -1,3 +1,4 @@
+# Задание 10
 list1_input = input("Введите элементы первого списка: ")
 list2_input = input("Введите элементы второго списка: ")
 start_idx = int(input("Введите начальный индекс диапазона: "))
@@ -6,7 +7,7 @@ end_idx = int(input("Введите конечный индекс диапазо
 lst1 = list(map(int, list1_input.split()))
 lst2 = list(map(int, list2_input.split()))
 
-elements_to_move = lst1[start_idx-1:end_idx+1]
+elements_to_move = lst1[start_idx:end_idx+1]
 lst2.extend(elements_to_move[::-1])
 new_lst1 = lst1[:start_idx] + lst1[end_idx+1:]
 
