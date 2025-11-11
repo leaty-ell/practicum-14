da2ab6ceac2a69ae5413968fe778fd4eea3a0e25
leaty-ell <lisalimanova14@gmail.com>
2025-11-11 08:56:43 +0700

    Update tsk5.py

--- tsk5.py
+++ tsk5.py
@@ -5,11 +5,10 @@ numbers_list = []
 for n in numbers:
     numbers_list.append(int(n))
 
-print("Список чисел:", numbers_list)
-
 total = 0
 for num in numbers_list:
     total += num
     
 average = total / len(numbers_list)
+print("Список чисел:", numbers_list)
 print("Среднее значение:", average)
